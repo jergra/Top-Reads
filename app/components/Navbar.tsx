6993faf9b46ac02174c707418e78c72538d4148b
jergra43 <jergra@yahoo.com>
2023-12-11 15:35:47 -0800

    navbar changes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,11 +3,11 @@ import ThemeButton from "./ThemeButton"
 
 function Navbar() {
   return (
-    <div className='max-w-4xl mx-auto px-4 sm:px-6 lg:px-8'>
+    <div className='max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 mt-3'>
         <div className='flex justify-between h-16'>
             <div className='flex justify-between items-center w-full'>
                 <Link href='/'>
-                    <h1 className='text-2xl font-medium'>
+                    <h1 className='text-5xl font-extrabold leading-9 tracking-tight textgray-900 dark:text-gray-100 sm:text-5xl sm:leading-10 md:text-5xl md:leading-14'>
                         Top <span className='text-teal-500'>Reads</span>
                     </h1>
                 </Link>
@@ -19,3 +19,6 @@ function Navbar() {
 }
 
 export default Navbar
+
+
+// text-3xl font-bold
--- app/page.tsx
+++ app/page.tsx
@@ -17,11 +17,12 @@ export default async function IndexPage() {
   return (
     <div className='divide-y divide-gray-200 dark:divide-gray-700'>
       <div className='space-y-2 pt-6 pb-8 md:space-y-5'>
-        <h1 className='text-3xl font-extrabold leading-9 tracking-tight textgray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14'>
-          All Posts
+        <h1 className='text-3xl font-extrabold leading-9 tracking-tight textgray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14'
+        >
+          
         </h1>
       </div>
-
+      <div>
       <ul>
         {data.map((post) => (
           <li key={post._id} className='py-4'>
@@ -49,6 +50,7 @@ export default async function IndexPage() {
           </li>
         ))}
       </ul>
+      </div>
     </div>
   )
 }
