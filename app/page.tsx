d3ae044ed6765a96022e79949efc3f492075dc6c
jergra43 <jergra@yahoo.com>
2024-01-24 19:11:05 -0800

    improve scrolling appearance

--- app/page.tsx
+++ app/page.tsx
@@ -20,41 +20,43 @@ export default async function IndexPage() {
         <h1 className='text-3xl font-extrabold leading-9 tracking-tight textgray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14'
         ></h1>
       </div>
-      <ul>
-        {data.map((post) => (
-          <li key={post._id} className='py-4'>
-            <article className='space-y-2 xl:grid xl:grid-cols-6 xl:items-baseline xl:space-y-0'>
-              <div>
-                <p className='text-base font-medium leading-6 text-teal-500'>
-                  {new Date(post._createdAt).toISOString().split('T')[0]}
-                </p>
-              </div>
+      <div className='h-[2000px] overflow-hidden overflow-y-scroll no-scrollbar'>
+        <ul>
+          {data.map((post) => (
+            <li key={post._id} className='py-4'>
+              <article className='space-y-2 xl:grid xl:grid-cols-6 xl:items-baseline xl:space-y-0'>
+                <div>
+                  <p className='text-base font-medium leading-6 text-teal-500'>
+                    {new Date(post._createdAt).toISOString().split('T')[0]}
+                  </p>
+                </div>
 
-              <Link 
-                href={post.url}
-                className='space-y-3 xl:col-span-5'
-              >
-                <div className='flex justify-between'>
-                  <div>
-                    <h3 className='text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100'>
-                      {post.title}
-                    </h3>
-                  </div>
-                  <div>
-                    <div className='text-teal-500 mt-1'>
-                      {post.author} <span className='text-gray-500 dark:text-gray-400'>in {post.publication}</span>
+                <Link 
+                  href={post.url}
+                  className='space-y-3 xl:col-span-5'
+                >
+                  <div className='flex justify-between'>
+                    <div>
+                      <h3 className='text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100'>
+                        {post.title}
+                      </h3>
+                    </div>
+                    <div>
+                      <div className='text-teal-500 mt-1'>
+                        {post.author} <span className='text-gray-500 dark:text-gray-400'>in {post.publication}</span>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <p className='prose max-w-none text-gray-500 dark:text-gray-400 line-clamp-6'>
-                  {post.content}
-                </p>
-              </Link>
-            </article>
-          </li>
-        ))}
-      </ul>
-      <hr className='divide-gray-200 dark:divide-gray-700 mt-10' />
+                  <p className='prose max-w-none text-gray-500 dark:text-gray-400 line-clamp-6'>
+                    {post.content}
+                  </p>
+                </Link>
+              </article>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <hr className='divide-gray-200 dark:divide-gray-700 mt-1' />
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,6 @@ const config: Config = {
   ],
   darkMode: 'class',
   theme: {},
-  plugins: [require("@tailwindcss/typography")],
+  plugins:  [require("@tailwindcss/typography")],
 }
 export default config
